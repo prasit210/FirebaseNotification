0aea6ccce0e7acbb0e6800bf3396b0c9d0fd6c5f
prasit210 <prasit562@gmail.com>
2018-04-12 21:51:18 +0700

    Shot by Firebase Console

---
+++ app/src/main/java/com/example/prasi/fibasenotification/activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.prasi.fibasenotification.activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.prasi.fibasenotification.R;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToken(View view){
+        Toast.makeText(getApplicationContext(), FirebaseInstanceId.getInstance().getToken(),Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/prasi/fibasenotification/service/MyFirebaseInstanceIdService.java
@@ -0,0 +1,16 @@
+package com.example.prasi.fibasenotification.service;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = "TOKEN";
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
